61fe664672c7915da40ab64f8ab2afb6ed70b4ea
Lightman <yaruk.my@gmail.com>
2016-04-06 14:49:21 +0300

    fix: fix #set(save)Group

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,11 @@ module.exports = {
 		var value = simpleStorage.get(key);
 		return (typeof value == 'undefined') ? defaultVal : value;
 	},
-	saveGroup: function(groupName, key, val) {
-		simpleStorage.set(key + groupName, val);
+	saveGroup: function(groupName, key, val, options) {
+		simpleStorage.set(key + groupName, val, options);
 	},
-	setGroup: function(groupName, key, val) {
-		this.set(key + groupName, val);
+	setGroup: function(groupName, key, val, options) {
+		this.set(key + groupName, val, options);
 	},
 	getGroup: function(groupName, key, defaultVal) {
 		return this.get(key + groupName, defaultVal);
